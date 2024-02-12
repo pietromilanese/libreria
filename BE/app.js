82135321b038a99ab313c83706f972ba7d0eba52
PietroMil <pietro.milanese@edu.itspiemonte.it>
2024-02-12 14:55:21 +0100

    added CORS library

--- BE/app.js
+++ BE/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { router } from './service.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
